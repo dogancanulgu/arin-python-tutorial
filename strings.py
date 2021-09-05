32c208ebf68f775dd0ef16365deb6f1e46fd2ad7
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-06 00:24:08 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,23 @@
+name = 'arin software'
+
+# print(len(name))
+
+# print(name[0:7])
+
+# print(name[:7])
+
+# print(name.title())
+
+# print(name.upper())
+
+# print(name.lower())
+
+# print(name.count('a'))
+
+# print(name.find('n'))
+
+# print(name.replace('software', 'yazılım'))
+
+print(dir(name))
+
+print(help(str))
