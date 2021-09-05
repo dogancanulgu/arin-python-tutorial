1bbe2136e8690f836c0275390ee7d864c08ccb93
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-06 00:24:00 +0300

    hello world

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+# print('Hello World')
+
+# print('merhaba')
+
+# x = 'merhaba'
+
+# print(x)
+
+# x = 'nasilsin'
+
+# print(x)
+
+# my_string = 'Hello World'
+
+# print(my_string)
+
+name = 'dogan'
+
+# message = 'merhaba ' + name 
+
+message = f'merhaba {name}'
+
+print(message)
+
+message = 'merhaba {}'
+
+print(message.format(name))
