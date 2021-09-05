0ea29ef25c33d8c8467db0c0f83d7d9760e6b392
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-06 01:12:15 +0300

    application

---
+++ application1.py
@@ -0,0 +1,50 @@
+# a = 14
+# a = 4.2
+# a = 8
+# a = 8 / 2
+# print(a)
+# print(type(a))
+
+# print( (8 - 4) * 5 + (5 % 2))
+
+
+# r = 5
+# pi = 3.14159
+# print(round(2 * pi * r, 3))
+# print(pi * r * r)
+# print(pi * (r**2))
+# print(pi * pow(r, 2))
+
+# import math
+# r=5
+# print(2 * math.pi * r)
+
+# x = 21
+# if(x % 2 == 0):
+#     print('X bir cift sayıdır')
+# else:
+#     print('X bir tek sayıdır')
+
+# print(abs(4-7) * (4 + 7))
+
+# x = 'Welcome to python code'
+# print(x)
+# print(type(x))
+
+# name = 'Dogan'
+# print('My name is ' + name)
+# print('My name is {}'.format(name))
+# print(f'My name is {name}')
+
+# name = 'Dogan'
+# age = 26
+# married = False
+# print(name, age, married)
+
+# x = '100'
+# y = 50
+# print(int(x) - y)
+
+my_string = input('İsminiz nedir?')
+print(my_string.upper())
+print(my_string.lower())
