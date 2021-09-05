94ba940167f5b3ade6e77e3f969e4a9689989668
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-09-06 00:37:16 +0300

    numbers

---
+++ numbers.py
@@ -0,0 +1,49 @@
+# x = 17
+# x = 17.6
+# x = '17'
+
+# print(type(x))
+
+# print(5 + 3)
+# print(5 - 3)
+# print(5 * 3)
+# print(5 / 3)
+# print(6 / 3)
+# print(5 // 3)
+# print(5 ** 3)
+# print(21 % 5)
+# print((5 -3) * 4 + 6)
+
+# number = -5
+# print(abs(number))
+
+# number = 6.78
+# print(float(number))
+# print(round(number, 1))
+
+# print(pow(3, 2))
+# print(pow(4, 3))
+
+# print(min(4, 3))
+# print(max(4, 3, 6, 11))
+
+# number = 4
+# # number = number + 2
+# number += 2
+# number *= 2
+# print(number)
+
+# num_1 = '24'
+# num_2 = '41'
+
+# num_1 = int(num_1)
+# num_2 = int(num_2)
+
+# print(num_1 + num_2)
+
+# age = 22
+# message = 'Ben ' + str(age) + ' yaşındayım'
+# print(message)
+
+my_string = 'test'
+print(my_string * 4)
